b6037abb09c9bf06b14207166ffe2fb8beb27111
tanakorn29 <tanakorn.naranong@gmail.com>
2018-07-30 00:25:04 +0700

    30072561

--- Clinic2018/Clinic2018/clinic_pharmacist_service.cs
+++ Clinic2018/Clinic2018/clinic_pharmacist_service.cs
@@ -462,7 +462,7 @@ namespace Clinic2018
 
 
             conn.Open();
-
+            //******
             string query = ("select treatment_record.treatr_id,opd.opd_name,treatment_record.treatr_medi_queue from opd inner join treatment_record on treatment_record.opd_id = opd.opd_id where treatr_status = 2 ORDER BY treatr_medi_queue ASC");
             cmd = new SqlCommand(query, conn);
             sda = new SqlDataAdapter(cmd);
